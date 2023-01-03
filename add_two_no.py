0e54683fda58f03c386e09086cc38a786158a634
ashutoshgaur0809 <121347667+ashutoshgaur0809@users.noreply.github.com>
2023-01-03 20:36:25 +0530

    Add files via upload

---
+++ add_two_no.py
@@ -0,0 +1,7 @@
+T = int(input("Enter No. o test cases - "))
+for i in range(T):
+    sum = 0
+    a = int(input("Enter No. a - "))
+    b = int(input("Enter No. b - "))
+    sum = a+b
+    print(sum)
